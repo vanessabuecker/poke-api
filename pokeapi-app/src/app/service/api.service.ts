1114d7d2c484584ccc329ef3573619bf0a08c958
VanessaBuecker <v.buecker@rocketmail.com>
2025-06-23 09:19:36 -0300

    feat: cria página de detalhes

--- pokeapi-app/src/app/service/api.service.ts
+++ pokeapi-app/src/app/service/api.service.ts
@@ -5,11 +5,16 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
+  private url: string = "https://pokeapi.co/api/v2/";
 
   constructor(private http: HttpClient) { }
 
   getPokeList(limit: number = 100) {
     return this.http.get(`${this.url}pokemon?limit=${limit}`);
+  }
+
+  getPokeDetails(nameOrId: string | number) {
+    return this.http.get(`${this.url}pokemon/${nameOrId}`);
   }
 }
 
