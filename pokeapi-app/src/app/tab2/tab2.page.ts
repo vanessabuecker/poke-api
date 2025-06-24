c2a21843a9188fd2ac2b083316aa35c73a7a3427
VanessaBuecker <v.buecker@rocketmail.com>
2025-06-24 08:57:54 -0300

    feat: adiciona experiencia, peso, altura , movimentos e sons à página de detalhes.

--- pokeapi-app/src/app/tab2/tab2.page.ts
+++ pokeapi-app/src/app/tab2/tab2.page.ts
@@ -20,8 +20,16 @@ export class Tab2Page {
           this.pokemons.push({
             name: details.name,
             image: details.sprites.front_default,
+            base_experience: details.base_experience,
+            height: details.height,
+            weight: details.weight,
             types: details.types.map((t: any) => t.type.name),
-            abilities: details.abilities.map((a: any) => a.ability.name)
+            abilities: details.abilities.map((a: any) => a.ability.name),
+            moves: details.moves.slice(0, 3).map((m: any) => m.move.name),
+            cry: {
+              latest: `https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/${details.id}.ogg`,
+              legacy: `https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/legacy/${details.id}.ogg`
+            }
           });
         });
       });
